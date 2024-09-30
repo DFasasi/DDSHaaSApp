e43318d6c9bae14336a18518a74182790dc10378
David Fasasi <168508938+DFasasi@users.noreply.github.com>
2024-09-30 00:51:12 -0500

    createHardwareSet Progress

--- server/hardwareDatabase.py
+++ server/hardwareDatabase.py
@@ -1,6 +1,11 @@
 # Import necessary libraries and modules
 from pymongo import MongoClient
 
+# Connect to MongoDB
+client = MongoClient("mongodb+srv://masterUser:iXshJM0Tn5C9aAYt@userinfo.qp9mr.mongodb.net/?retryWrites=true&w=majority&appName=UserInfo")
+db = client["hardwareDB"]  # Define the database
+hw_collection = db["hardwareSets"]  # Define the collection
+
 '''
 Structure of Hardware Set entry:
 HardwareSet = {
@@ -13,6 +18,15 @@ HardwareSet = {
 # Function to create a new hardware set
 def createHardwareSet(client, hwSetName, initCapacity):
     # Create a new hardware set in the database
+    HardwareSet = {
+        'hwName': hwSetName,
+        'capacity': initCapacity,
+        'availability': initCapacity
+    }
+    
+    # Insert the hardware set into the collection
+    hw_collection.insert_one(HardwareSet)
+    print(f"Hardware set '{hwSetName}' created with capacity {initCapacity}")
     pass
 
 # Function to query a hardware set by its name
